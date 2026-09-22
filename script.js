0e312db3713d924951668260911d8faa77e549b9
rooandsonevangelista-hue <rooandson.evangelista@gmail.com>
2026-09-22 20:17:08 -0300

    Implement task counter in addTask and deleteTask
    Added task counter functionality to update title dynamically.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const { createElement } = require("react");
+let count = 0;
 
 function addTask(){
     let input = document.getElementById("taskInput");
@@ -7,6 +7,12 @@ function addTask(){
     if(task == ''){
         return;
     }
+
+    //CONTADOR DO TÍTULO
+
+    let titulo = document.getElementById("title");
+
+
 
     let listItem = document.createElement("li");
 
@@ -30,7 +36,13 @@ function addTask(){
     deleteButton.textContent = "deletar";
     deleteButton.className = "deleteButton";
     deleteButton.onclick = function(){
-        listItem.remove();
+                   
+            listItem.remove();
+            subUpdate(title);
+            if(count == 0){
+                titulo.textContent = "Minha Lista de Tarefas:" + "(A lista está vazia)";
+            }
+                  
     }
 
     let deleteIcon = document.createElement("i");
@@ -50,12 +62,35 @@ function addTask(){
 
     input.value = "";
 
-
+    sumUpdate(title);
 }
 
 function deleteTask(){
     let list = document.getElementById("taskList");
     list.remove();
+    
+    let titulo = document.getElementById("title");
+    zeroUpdate(title);
 
+}
 
+function sumUpdate(titulo){
+
+    count++;
+    titulo.textContent = "Minha Lista de Tarefas:" + "(" + count +")";
+
+}
+
+function subUpdate(titulo){
+    count--;
+    titulo.textContent = "Minha Lista de Tarefas:" + "(" + count +")";
+
+}
+
+function zeroUpdate(titulo){
+    count = 0;
+    titulo.textContent = "Minha Lista de Tarefas:" + "(" + count +")";
+    if(count == 0){
+       titulo.textContent = "Minha Lista de Tarefas:" + "(A lista está vazia)";
+   }
 }
